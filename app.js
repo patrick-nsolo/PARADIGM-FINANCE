9ac8e9735b276465f4ed98e9404538af63a6bb99
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-01-16 13:37:24 +0100

    team css commit

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-let button = document.getElementById("dropdown-button");
-let dropdown = document.getElementById("dropdown-menu");
+// Get the button and the dropdown menu
+let button = document.getElementById("dropdown-btn");
+let dropdown = document.getElementById("dropdown-info");
 
-button.addEventListener("mouseover", function(){
-    dropdown.style.display = "block";
-});
+// Set the initial state of the dropdown menu to be hidden
+dropdown.style.display = "none";
 
-button.addEventListener("mouseout", function(){
-    dropdown.style.display = "none";
+// Add an event listener to the button to toggle the dropdown menu when clicked
+button.addEventListener("click", function() {
+    if (dropdown.style.display === "none") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
 });
