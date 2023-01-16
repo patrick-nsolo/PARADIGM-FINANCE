f32660ee3be0af74bf1fe3fb3a0914d1d2d4ac67
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-01-16 15:38:57 +0100

    update paradigm team page

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-// Get the button and the dropdown menu
-let button = document.getElementById("dropdown-btn");
-let dropdown = document.getElementById("dropdown-info");
 
-// Set the initial state of the dropdown menu to be hidden
-dropdown.style.display = "none";
-
-// Add an event listener to the button to toggle the dropdown menu when clicked
-button.addEventListener("click", function() {
-    if (dropdown.style.display === "none") {
-        dropdown.style.display = "block";
+function myFunction() {
+    var x = document.getElementById("chairman-dropdown, mildred-dropdown");
+    if (x.style.display === "none") {
+      x.style.display = "block";
     } else {
-        dropdown.style.display = "none";
+      x.style.display = "none";
     }
-});
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
