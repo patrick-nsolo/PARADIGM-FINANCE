d09ba0c187239615332ca83e48c88bd60b9b2b5b
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-01-19 11:10:41 +0100

    Initial commit for loan calculator

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//TEAM PAGE 
 function myFunction(id) {
     var dropdown = document.getElementById(id);
     if (dropdown.style.display === "block"){
@@ -8,6 +8,16 @@ function myFunction(id) {
     }
 
 }
+
+//LOAN FORM 
+function calculatePayment() {
+    let loanAmount = document.getElementById("loan-amount").value;
+    let interestRate = document.getElementById("interest-rate").value / 100 / 12;
+    let loanTerm = document.getElementById("loan-term").value * 12;
+    let monthlyPayment = loanAmount * interestRate / (1 - Math.pow(1 + interestRate, -loanTerm));
+    
+    document.getElementById("monthly-payment").value = monthlyPayment.toFixed(2);
+  }
     
 
 
